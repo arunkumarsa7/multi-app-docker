b83594fec022daaa3a6614623dce0ddd33e0b2de
Arun Kumar S A <arunkumarsa7@gmail.com>
2023-03-16 22:17:19 +0100

    Backend auto update and env variables

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://arun:admin@mongo-db:27017/course-goals?authSource=admin',
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongo-db:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
